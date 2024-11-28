f11e9999fd6948611a9be974cdd9d5063ede55ce
DjSpykid <opanugaaladetomiwa.com>
2024-11-28 21:45:18 +0100

    create the about page

---
+++ spymovie-app/src/app/about/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>
+        loading.........................
+      </p>
+    </div>
+  )
+}
+
+export default About
